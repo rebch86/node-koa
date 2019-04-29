79fe6a516e0ce7ce01db8c43be5c9fbba9bd3461
kyle <kyle@freshcode.me>
2019-04-29 13:00:31 +0900

    패스포트 작업

--- src/koaServer.js
+++ src/koaServer.js
@@ -1,11 +1,28 @@
 const Koa = require("koa");
 const app = new Koa();
 
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy; // local 로그인 전략
+const jwtStrategy = require('passport-jwt').Strategy; // jwt 로그인 전략
+
+
+// usernameField / passwordField는 어떤 폼 필드로부터 아이디와 비밀번호를 전달받을 지 설정
+// session -> 세션 저장여부
+passport.use(new localStrategy({
+    usernameField : 'userId',
+    passwordField : 'password'
+},
+    function (userId, password, cb) {
+        //this one is typically a DB call. Assume that the returned user object is pre-formatted and ready for storing in JWT
+    }
+));
+
+
 app.use(async ctx => {
     ctx.body = "<h1>First koa example !!</h1>";
 });
 
-const port = 8080;
+const port = 3030;
 app.listen(port, function () {
     console.log('koa start..');
     console.log(`PORT : ${port}`);
