da62e52f4ca76d5b3a44b8f00485c75b446aae5b
kyle <kyle@freshcode.me>
2019-04-29 14:03:40 +0900

    패스포트 작업

--- src/koaServer.js
+++ src/koaServer.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const localStrategy = require('passport-local').Strategy; // local 로그인 전략
 const jwtStrategy = require('passport-jwt').Strategy; // jwt 로그인 전략
 
-
+// https://medium.com/front-end-weekly/learn-using-jwt-with-passport-authentication-9761539c4314
 // usernameField / passwordField는 어떤 폼 필드로부터 아이디와 비밀번호를 전달받을 지 설정
 // session -> 세션 저장여부
 passport.use(new localStrategy({
